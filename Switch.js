13738c6a980ea30b20a0773ca49e9690d6d49e64
Nadooor <nadersayed742@gmail.com>
2025-12-08 06:14:22 +0200

    Finished all the tables and left two for later develpment

--- Switch.js
+++ Switch.js
@@ -10,10 +10,14 @@ AFRAME.registerComponent('switch', {
         astrocount = -1;
         const Food = ['#Chips', '#Donuts', '#Fish', '#Turkey', '#Cupcake', '#Croissant', '#IceCream'];
         foodcount = -1;
-        const Galla = ['#snakeboard', '#Current', '#here', '#Hirobo', '#Patric']
+        const Galla = ['#snakeboard', '#Current', '#Snakepad', '#SnakeZero' ,'#Hirobo', '#Patric']
         Gallacount = -1;
         const Spooky = ['#TeenGirl', '#Skeleton', '#DamagedWall', '#Pumpkin', '#HalloweenChar', '#Monster', '#Snowman', '#House', '#Mouth', '#momwagy']
         spookycount = -1; 
+        const Fall = ['#ball', '#bird', '#girl', '#hornet', '#pat', '#tree', '#treerock'];
+        Fallcount = -1;
+        const Winter = ['#Campfire', '#snowfloor', '#snowtrees' ] 
+        Wintercount = -1;
         const world = this.el.getAttribute('id')
     this.el.addEventListener('click', (e) => {
         console.log("clicked");
@@ -31,8 +35,10 @@ AFRAME.registerComponent('switch', {
             }
             }
             console.log("Astroed");
+            const astrotext = document.querySelector('#AstroText');
             const Model = document.querySelector('#astroMODELS');
             Model.setAttribute('gltf-model', astro[astrocount]);
+            astrotext.setAttribute('value', astro[astrocount].replace('#', ''));
             if (astro[astrocount] == '#Sun'){
                 Model.setAttribute('scale', "0.08 0.08 0.08");
                 Model.setAttribute('position', "0 0 0");
@@ -79,8 +85,10 @@ AFRAME.registerComponent('switch', {
                 foodcount = 0;
             }
             }
+            const FoodText = document.querySelector('#FoodText');
             const Model = document.querySelector('#foodMODELS');
             Model.setAttribute('gltf-model', Food[foodcount]);
+            FoodText.setAttribute('value', Food[foodcount].replace('#', ''));
             if (Food[foodcount] == '#Chips'){
                 Model.setAttribute('scale', "3 3 3");
             } else if (Food[foodcount] == '#Fish'){
@@ -117,16 +125,17 @@ AFRAME.registerComponent('switch', {
                 Gallacount = 0;
             }
             }
-
+            const GallaText = document.querySelector('#GallaText');
             const Model = document.querySelector('#gallaMODELS');
             Model.setAttribute('gltf-model', Galla[Gallacount]);
+            GallaText.setAttribute('value', Galla[Gallacount].replace('#',''));
             if (Galla[Gallacount] == '#snakeboard'){
                 Model.setAttribute('scale', '0.006 0.006 0.006 ');
                 Model.setAttribute('position', '0 1.7 0');
             } else if (Galla[Gallacount] == '#Current'){
                 Model.setAttribute('scale', '0.006 0.006 0.006 ');
                 Model.setAttribute('position', '0 1.7 0');
-            } else if (Galla[Gallacount] == '#here'){
+            } else if (Galla[Gallacount] == '#SnakePad'){
                 Model.setAttribute('scale', '0.006 0.006 0.006 ');
                 Model.setAttribute('position', '0 1.7 0');
             } else if (Galla[Gallacount] == '#Hirobo'){
@@ -135,14 +144,19 @@ AFRAME.registerComponent('switch', {
             } else if (Galla[Gallacount] == '#Patric'){
                 Model.setAttribute('scale', '0.7 0.7 0.7');
                 Model.setAttribute('position', '0 1.7 0');
+
+            } else if (Galla[Gallacount] == '#SnakeZero') {
+
             }
             console.log(Galla[Gallacount]);
         }
         //#endregion Galla
-        else if (world == 'Winter'){
-            console.log("Wintered");
-            
+        // #region Puzz(Under-maintenance)
+        else if (world == 'Puzz'){
+            console.log("Puzzed");
+
         }
+        //#endregion Puzz
         // #region Spooky
         else if (world == 'Spooky'){
             console.log("Spooked");
@@ -157,8 +171,10 @@ AFRAME.registerComponent('switch', {
                 spookycount = 0;
             }
             }
+            const SpookyText = document.querySelector('#SpookyText');
             const Model = document.querySelector('#spookyMODELS');
             Model.setAttribute('gltf-model', Spooky[spookycount]);
+            SpookyText.setAttribute('value', Spooky[spookycount].replace('#', ''));
             console.log(spookycount);
             console.log(Spooky[spookycount]);
             if (Spooky[spookycount] == '#DamagedWall'){
@@ -184,10 +200,74 @@ AFRAME.registerComponent('switch', {
             }
         } 
         // #endregion Spooky
-        else if (world == 'Loopy'){
-            console.log("Loopied");
-        } else if (world == 'Puzz'){
-            console.log("Puzzed");
+        // #region Fall
+        else if (world == 'Fall'){
+            console.log("Falled");
+            if (this.data.dir == '+'){
+            Fallcount++; 
+            if (Fallcount > 6){
+                Fallcount = 6;
+            }
+            } else if (this.data.dir == '-'){
+            Fallcount--;
+            if (Fallcount < 0){
+                Fallcount = 0;
+            }
+            }
+            const FallText = document.querySelector('#FallText');
+            const Model = document.querySelector('#fallMODELS');
+            Model.setAttribute('gltf-model', Fall[Fallcount]);
+            FallText.setAttribute('value', Fall[Fallcount].replace('#', ''));
+            console.log(Fallcount);
+            console.log(Fall[Fallcount]);
+            if (Fall[Fallcount] == '#ball'){
+                Model.setAttribute('scale', '0.6 0.6 0.6');
+            } else if (Fall[Fallcount] == '#bird'){
+                Model.setAttribute('scale', '1 1 1');
+            } else if (Fall[Fallcount] == '#girl'){
+                Model.setAttribute('scale', '1 1 1');
+            } else if (Fall[Fallcount] == '#hornet'){
+                Model.setAttribute('scale', '1 1 1');
+            } else if (Fall[Fallcount] == '#pat'){
+                Model.setAttribute('scale', '0.8 0.8 0.8');
+            } else if (Fall[Fallcount] == '#tree'){
+                Model.setAttribute('scale', '0.3 0.3 0.3');
+            } else if (Fall[Fallcount] == '#treerock'){
+                Model.setAttribute('scale', '0.05 0.05 0.05');
+            }
+        } 
+        // #endregion Fall
+        else if (world == 'Winter'){
+            console.log("Wintered");
+            if (this.data.dir == '+'){
+            Wintercount++; 
+            if (Wintercount > 3){
+                Wintercount = 3;
+            }
+            } else if (this.data.dir == '-'){
+            Wintercount--;
+            if (Wintercount < 0){
+                Wintercount = 0;
+            }
+            }
+
+            const WinterText = document.querySelector('#WinterText');
+            const Model = document.querySelector('#winterMODELS');
+            Model.setAttribute('gltf-model', Winter[Wintercount]);
+            WinterText.setAttribute('value', Winter[Wintercount].replace('#',''));
+            console.log(Wintercount);
+            console.log(Winter[Wintercount]);
+            if (Winter[Wintercount] == '#Campfire'){
+                Model.setAttribute('scale', '0.08 0.08 0.08');
+                Model.setAttribute('position', '0 1.5 0');
+            } else if (Winter[Wintercount] == '#snowfloor'){
+                Model.setAttribute('scale', '0.1 0.1 0.1');
+                Model.setAttribute('position', '0 1.5 0');
+
+            } else if (Winter[Wintercount] == '#snowtrees'){
+                Model.setAttribute('scale', '0.7 0.7 0.7'); 
+                Model.setAttribute('position', '0 1.5 0');
+            }
         }
 
     });    
